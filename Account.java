eec91d374568e99c44fa8b6b4a5cd11895b64cae
Taylor Huston <taylor.a.huston@gmail.com>
2015-05-18 02:59:44 -0700

    Add README

--- Account.java
+++ Account.java
@@ -16,29 +16,29 @@ public class Account {
         } else {
             this.balance = 0.0;
         }
-    } //End Constructor
+    }
     
     public Account() {
         this.name = null;
         this.balance = 0.0;
-    } //End Constructor
+    } 
 
     //Setters and Getters
     public void setName(String newName) {
         this.name = newName;
-    } //End SetName
+    }
     
     public String getName() {
         return this.name;
-    } //End getName
+    }
 
     public void deposit(double toDeposit) {
         if (toDeposit > 0.0) {
             this.balance += toDeposit;
         }
-    } //End deposit
+    } 
     
     public double getBalance() {
         return this.balance;
-    } //End getBalance
+    } 
 } //End Account
---
+++ README.md
@@ -0,0 +1,4 @@
+#Account
+
+ Very basic program to demonstrate accessing fields in a class
+ From <a href="http://www.amazon.com/Java-Program-Early-Objects-Edition/dp/0133807800">Java How To Program, 10/e, Early Objects - Chapter 3</a>
